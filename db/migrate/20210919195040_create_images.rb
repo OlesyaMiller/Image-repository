e0d8df49ca69b03c3e6f21ed7799d816a8b99374
Olesya Miller <olesyam.miller@gmail.com>
2021-09-19 16:40:38 -0400

    added description attribute to the image model

--- db/migrate/20210919195040_create_images.rb
+++ db/migrate/20210919195040_create_images.rb
@@ -2,6 +2,7 @@ class CreateImages < ActiveRecord::Migration[6.0]
   def change
     create_table :images do |t|
       t.string :name
+      t.string :description
       t.integer :user_id
 
       t.timestamps
