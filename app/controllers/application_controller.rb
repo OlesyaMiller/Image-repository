4bd750b3b32029966d52525411176c84f7c2e4aa
Olesya Miller <olesyam.miller@gmail.com>
2021-09-19 17:38:20 -0400

    added methods to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    helper_method :current_user, :logged_in?
+    
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+    
+    def logged_in?
+        !!current_user
+    end
+
+    def require_login
+        if current_user 
+            current_user
+        else 
+            redirect_to root_path
+        end 
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_many_attached :images
     has_many :comments
+    has_secure_password
+    validates :username, :email, :password_digest, presence: true, uniqueness: true
 end
