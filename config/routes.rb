374657713df835a91c874f8533618d87306718d1
Olesya Miller <olesyam.miller@gmail.com>
2021-09-19 16:38:39 -0400

    set up the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  root 'static#home'
+
   resources :images
   resources :users
+  resources :users do
+    resources :images 
+  end
+
+  get '/signin', to: "session#signin"
+  post '/signin', to: "session#create"
+  get '/logout', to: "session#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
