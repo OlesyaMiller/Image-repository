c9d13f4d9a64069bb6e2be0f9413eddd17624362
Olesya Miller <olesyam.miller@gmail.com>
2021-09-19 16:34:05 -0400

    created session controller

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,23 @@
+class SessionController < ApplicationController
+    class SessionController < ApplicationController
+
+        def new  
+        end
+    
+        def create
+            @user = User.find_by(username: params[:username]) 
+            if @user && @user.authenticate(params[:password])
+                session[:user_id] = @user.id 
+                redirect_to user_path(@user)
+            else
+                render :signin
+            end
+        end
+    
+        def destroy
+            session.delete("user_id") 
+            redirect_to root_path
+        end
+    
+    end
+end
